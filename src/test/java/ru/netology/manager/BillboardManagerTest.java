cb69f1d4240ca935238b8b0b0e3068ff8f15a011
Kostromina Alina <a.kstrmna95@gmail.com>
2023-11-01 11:44:18 +0700

    Some tests failed

--- src/test/java/ru/netology/manager/BillboardManagerTest.java
+++ src/test/java/ru/netology/manager/BillboardManagerTest.java
@@ -55,9 +55,180 @@ public class BillboardManagerTest {
         manager.add(sixth);
         manager.add(seventh);
 
-        Movie[] expected = {first, second, third, fourth, fifth, sixth, seventh };
+        Movie[] expected = {first, second, third, fourth, fifth, sixth, seventh};
         Movie[] actual = manager.findAll();
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void mustShowUnderLimitWithDefaultNumber() { // меньше фильмов, чем лимит, с дефолтным значением
+        BillboardManager manager = new BillboardManager();
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+
+        Movie[] expected = {first, second, third, fourth};
+        Movie[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void mustShowAsLimitWithDefaultNumber() { // столько же фильмов, как лимит, с дефолтным значением
+        BillboardManager manager = new BillboardManager();
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+
+        Movie[] expected = {first, second, third, fourth, fifth};
+        Movie[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void mustShowOverLimitWithCertainNumber() { // больше фильмов, чем лимит, с устанавливаемым значением
+        BillboardManager manager = new BillboardManager(6);
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+        manager.add(sixth);
+        manager.add(seventh);
+
+        Movie[] expected = {first, second, third, fourth, fifth, sixth, seventh};
+        Movie[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void mustShowUnderLimitWithCertainNumber() { // меньше фильмов, чем лимит, с устанавливаемым значением
+        BillboardManager manager = new BillboardManager(6);
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+
+        Movie[] expected = {first, second, third, fourth, fifth};
+        Movie[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void mustShowAsLimitWithCertainNumber() { // столько же фильмов, как лимит, с устанавливаемым значением
+        BillboardManager manager = new BillboardManager(6);
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+        manager.add(sixth);
+
+        Movie[] expected = {first, second, third, fourth, fifth, sixth};
+        Movie[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void mustShowOverLimitWithDefaultNumberReverseOrder() { // больше фильмов, чем лимит, с дефолтным значением
+        BillboardManager manager = new BillboardManager();
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+        manager.add(sixth);
+        manager.add(seventh);
+
+        Movie[] expected = {seventh, sixth, fifth, fourth, third, second, first};
+        Movie[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void mustShowUnderLimitWithDefaultNumberReverseOrder() { // меньше фильмов, чем лимит, с дефолтным значением
+        BillboardManager manager = new BillboardManager();
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+
+        Movie[] expected = {fourth, third, second, first};
+        Movie[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void mustShowAsLimitWithDefaultNumberReverseOrder() { // столько же фильмов, как лимит, с дефолтным значением
+        BillboardManager manager = new BillboardManager();
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+
+        Movie[] expected = {fifth, fourth, third, second, first};
+        Movie[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void mustShowOverLimitWithCertainNumberReverseOrder() { // больше фильмов, чем лимит, с устанавливаемым значением
+        BillboardManager manager = new BillboardManager(6);
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+        manager.add(sixth);
+        manager.add(seventh);
+
+        Movie[] expected = {sixth, fifth, fourth, third, second, first};
+        Movie[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void mustShowUnderLimitWithCertainNumberReverseOrder() { // меньше фильмов, чем лимит, с устанавливаемым значением
+        BillboardManager manager = new BillboardManager(6);
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+
+        Movie[] expected = {fifth, fourth, third, second, first};
+        Movie[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void mustShowAsLimitWithCertainNumberReverseOrder() { // столько же фильмов, как лимит, с устанавливаемым значением
+        BillboardManager manager = new BillboardManager(6);
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+        manager.add(sixth);
+
+        Movie[] expected = {sixth, fifth, fourth, third, second, first};
+        Movie[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
     }
 }
 
